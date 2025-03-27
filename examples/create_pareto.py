c9a7354fdbfcf6deac02a821a3c5ebcfa1132646
MaartenHaineDiabatix <maarten.haine@diabatix.com>
2025-03-27 13:57:32 +0100

    Header not supported for file upload fixed

--- examples/create_pareto.py
+++ examples/create_pareto.py
@@ -1,24 +1,26 @@
 #-------------------------------------------------------------------------------#
 #
-#                         --- p y C o l d S t r e a m ---
+#                         --- C O L D S T R E A M api ---
+#
+#                                    EXAMPLE
 #
 #-------------------------------------------------------------------------------#
 
 import matplotlib.pyplot as plt
-from coldstream import ColdstreamSession
+from pycoldstreamv0_3.coldstream import ColdstreamSession
 
 session = ColdstreamSession.create_from_login(user="admin@admin.com",
                                               password="admin",
                                               host="eu1")
 
-projectID = 12345
-caseID = 12345
+projectID = 1550
+caseID = 5429
 
 P = session.projects.get_project(projectID)
 C = P.get_case(caseID)
 
 # Collect the data
-C = P.get_case(case_ID)
+C = P.get_case(caseID)
 
 for t in C.get_results_evolution_graph().data["targetGraphs"]:
     if t["targetType"] == "pressureLoss":
@@ -26,7 +28,7 @@ for t in C.get_results_evolution_graph().data["targetGraphs"]:
         break
 y = [r_i["value"] for r_i in C.get_results_evolution_graph().data["overallDesignPerformanceGraph"]["results"]]
 
-plt.plot(x, y, marker='+', linestyle='', label=tag)
+plt.plot(x, y, marker='+', linestyle='', label="title pareto")
 
 # Add grid and legend
 plt.grid(True)
@@ -34,5 +36,7 @@ plt.legend()
 plt.xlabel(r"$\Delta P$")
 plt.ylabel(r"$J$")
 
+plt.ticklabel_format(style="plain", axis="y", useOffset=False, useMathText=False)
 # Save the figure as a PNG file
+plt.tight_layout()
 plt.savefig("pareto.png", dpi=300)
